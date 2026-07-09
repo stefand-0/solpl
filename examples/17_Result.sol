67379a157198f36bc9b8ded5413fa48485dfa7ab
stefpga <stefandragan95@gmail.com>
2026-07-09 19:27:08 +0300

    Add divide function with error handling

---
+++ examples/17_Result.sol
@@ -0,0 +1,12 @@
+; A function that could fail
+divide(a, b)
+  b == 0 ? _return -> Result.err("Cannot divide by zero!")
+  _return -> Result.ok(a : b)
+end
+main()
+  _get("std/extdata/Result.sol")
+  // Usage:
+  res -> divide(10, 0)
+  res.status == 0 ? _out -> "Error: " + res.error
+  res.status == 1 ? _out -> "Success: " + res.data
+end
