04bbed913f7980a3f155c1d3f271830803cd74b1
stefpga <stefandragan95@gmail.com>
2026-07-09 19:20:40 +0300

    Add optional user lookup example in 16_Optionals.sol

---
+++ examples/16_Optionals.sol
@@ -0,0 +1,11 @@
+find_user(id)
+  id == 1 ? _return -> Maybe.some("Guy")
+  _return -> Maybe.none()
+end
+main()
+  _get("std/extdata/Maybe.sol")
+  ; For example, checking for an ID.
+  result -> find_user(1)
+  result.status == 0 ? _out -> "User not found!"
+  result.status == 1 ? _out -> "User found: " + result.value 
+end
