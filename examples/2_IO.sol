a1866ffaeadb3adc3400ee717a019153185d3c6b
stefpga <stefandragan95@gmail.com>
2026-07-09 12:18:57 +0300

    Refactor input handling in 2_IO.sol
    Updated input handling to use non-blocking I/O with pipes.

--- examples/2_IO.sol
+++ examples/2_IO.sol
@@ -1,8 +1,10 @@
 main()
   ; _out outputs a value to the console
   _out -> "Hello, World!"
-  ; _in() takes input with a prompt
-  _in("Name? ") -> name
+  ; _in takes input with a prompt
+  ; pipes the output into name
+  ; io is non-blocking
+  "Name? " >> _in >> name
   _out -> "Hello, " + name + "!"
   ; String concatenation is done with "+"
 end
