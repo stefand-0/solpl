530a253ceb1a053f8286aa82a88311b4f7272401
stefpga <stefandragan95@gmail.com>
2026-07-09 12:03:55 +0300

    Add basic arithmetic operations in 3_Arithmetic.sol

---
+++ examples/3_Arithmetic.sol
@@ -0,0 +1,10 @@
+main()
+  addition -> 1 + 1
+  subtraction -> 1 - 1
+  multiplication -> 1 * 1
+  division -> 1 : 1
+  _out -> addition
+  _out -> subtraction
+  _out -> multiplication 
+  _out -> division
+end
