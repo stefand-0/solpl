8d0dae3aed6f2bd39ed3badb275ffe922206a325
stefpga <stefandragan95@gmail.com>
2026-07-09 17:05:16 +0300

    Add example for string operations in 12_Strings.sol

---
+++ examples/12_Strings.sol
@@ -0,0 +1,19 @@
+main()
+  ; Here, we are going to check if a string is empty.
+  _get("std/str/String.sol")
+  string -> ""
+  isEmpty -> String.empty(string)
+  isEmpty == 1 ? _out -> "Empty!"
+
+  ; Now, checking if a String list contains a character.
+  list<ls>
+  _add(ls, 1, "A")
+  _add(ls, 2, "B")
+  hasChar -> String.contains_char("A")
+  hasChar == 1 ? _out -> "Has A!"
+
+  ; There are many more methods in String.sol
+  ; Now, let's make a JSON Key:Value
+  myKV -> String.to_json_kv("name", "guy")
+  _out -> myKV
+end
