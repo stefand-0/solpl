bcf05a325b15f911814a2253660b01eadd52debd
stefpga <stefandragan95@gmail.com>
2026-07-11 18:54:31 +0300

    Update 12_Strings.sol

--- examples/12_Strings.sol
+++ examples/12_Strings.sol
@@ -3,14 +3,14 @@ main()
   _get("std/str/String.sol")
   string -> ""
   isEmpty -> String.empty(string)
-  isEmpty == 1 ? _out -> "Empty!"
+  isEmpty = 1 ? _out -> "Empty!"
 
   ; Now, checking if a String list contains a character.
   list<ls>
   _add(ls, 1, "A")
   _add(ls, 2, "B")
   hasChar -> String.contains_char("A")
-  hasChar == 1 ? _out -> "Has A!"
+  hasChar = 1 ? _out -> "Has A!"
 
   ; There are many more methods in String.sol
   ; Now, let's make a JSON Key:Value
