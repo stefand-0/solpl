a63bf2aa46e0a29385a06c4eabda741aa81be5ee
stefpga <stefandragan95@gmail.com>
2026-07-11 20:26:13 +0300

    Update 10_Imports.sol

--- examples/10_Imports.sol
+++ examples/10_Imports.sol
@@ -2,7 +2,7 @@
 main()
   ; Always import in the main function
   ; For example, using the Math library
-  _get("std/calc/Math.sol")
+  _get("solstd/std/calc/Math.sol")
   ; The name of the lib file is the namespace
   square -> Math.sq(6)
   _out -> square
