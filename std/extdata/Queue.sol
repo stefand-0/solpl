1140222be6e127895aca6052cd66c4ecc623009d
stefpga <stefandragan95@gmail.com>
2026-07-11 18:43:12 +0300

    Update Queue.sol

--- std/extdata/Queue.sol
+++ std/extdata/Queue.sol
@@ -3,7 +3,7 @@ enqueue(list, current_tail_idx, value)
 end
 
 dequeue(list, current_front_idx, current_tail_idx)
-    if current_front_idx == current_tail_idx
+    if current_front_idx = current_tail_idx
         _return -> "QUEUE_EMPTY"
     else
         ; Return the front item, developer will increment front_idx by 1!
