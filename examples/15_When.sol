5d0a40135aefb460d3a0d957d01d404ecaf88e14
stefpga <stefandragan95@gmail.com>
2026-07-11 20:26:51 +0300

    Update 15_When.sol

--- examples/15_When.sol
+++ examples/15_When.sol
@@ -1,5 +1,5 @@
 main()
-  _get("std/extcond/Cond.sol")
+  _get("solstd/std/extcond/Cond.sol")
   ; The while loop operates until max uint64
   _out -> Cond.when(1, 0, "Returns... nothing? 1 != 0, and it never will")
 end
