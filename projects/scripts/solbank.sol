dfb22329fab6348eae1e7e95d14de6d56f10f224
stefpga <stefandragan95@gmail.com>
2026-07-20 19:32:05 +0300

    Create solbank.sol

---
+++ projects/scripts/solbank.sol
@@ -0,0 +1,27 @@
+account{}
+  name = ""
+  id = ""
+  cvv = ""
+  amount = ""
+end
+
+get_amount()
+  _return -> account.amount
+end
+
+verify_cvv()
+  _get("solstd/std/extdata/Boolean.sol")
+  temp -> account.cvv
+  if temp = 123
+    _return -> Boolean.true()
+  end
+end
+
+main()
+  verify_cvv()
+  balance = get_amount()
+  if balance < 0
+    debt = 0 - balance
+    _out -> "You are in debt! Please pay: " + debt + "!" 
+  end
+end
