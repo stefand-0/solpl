83f2406bb0ec25030ad8960f46dac0c531de15e4
stefpga <stefandragan95@gmail.com>
2026-07-20 19:39:54 +0300

    Create newio.sol

---
+++ solplans/newio.sol
@@ -0,0 +1,13 @@
+// I/O is part of the built in std library
+
+std.outln("Hello!")
+// with nl ^
+
+// without nl 
+std.out("No newline!")
+
+imm input: integer -> std.in("Age? ")
+
+// Age? 10
+// input = 10
+
