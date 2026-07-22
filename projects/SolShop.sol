00e6251cf1cef769e696845d91d3935b87b856ba
stefpga <stefandragan95@gmail.com>
2026-07-22 13:08:37 +0300

    Create SolShop.sol with user and shop definitions
    Added a basic structure for an online shop with user and shop data types.

---
+++ projects/SolShop.sol
@@ -0,0 +1,18 @@
+/*
+Better example for an online shop
+*/
+
+templ User
+  id: int32
+  name: string
+  basket: list
+end
+
+data Shop
+  name: string
+  products: list
+end
+
+fun main(): nothing
+  
+end
