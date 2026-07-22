887155ccef6a87db7358702b21a2b4c4440ad7e9
stefpga <stefandragan95@gmail.com>
2026-07-22 13:36:01 +0300

    Update SolShop.sol

--- projects/SolShop.sol
+++ projects/SolShop.sol
@@ -1,5 +1,6 @@
 /*
-Better example for an online shop
+Better* example for an online shop
+* = not really
 */
 
 templ User
@@ -13,6 +14,29 @@ data Shop
   products: list
 end
 
+fun register(): User
+  imm username: string -> std.in("--- Username: ")
+  imm loginID: int32 -> std.in("--- Login ID: ")
+  imm New: User -> User(loginID, username, [])
+  ret New
+end
+
+fun openShop(input: string): Shop
+  if (input == "Sol")
+    imm NewShop: Shop -> Shop("Sol", ["Statue", "Guy", "Dolphin with 1 eye"])
+  end
+  ret NewShop
+end
+
+fun addToBasket(product: string): list
+  imm newUser: User -> register()
+  list.push(newUser.basket, product)
+end
+
 fun main(): nothing
-  
+  std.outln("--- Shops: Sol")
+  imm shopInput: string -> std.in("What shop would you like to enter?")
+  imm Sol: Shop -> openShop("Sol")
+  addToBasket(Sol.products[1])
+  std.outln("In Basket: " + type.toString(Sol.products))
 end
